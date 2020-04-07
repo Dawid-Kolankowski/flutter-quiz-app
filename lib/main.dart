885f1268be40ed143d844c0916696e401a2612a3
unknown <nexog1997@gmail.com>
2020-04-07 17:58:38 +0200

    Added widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(App());
 
@@ -13,52 +13,68 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'white', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite car?',
+      'answers': [
+        {'text': 'Porsche', 'score': 2},
+        {'text': 'Toyota', 'score': 8},
+        {'text': 'Lamborghini', 'score': 1},
+        {'text': 'Honda', 'score': 10}
+      ]
+    },
+  ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
   }
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers':[
-          'Black','Red','Green','White'
-        ]
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers':[
-          'Rabbit','Snake','Elephant','Lion'
-        ]
-      },
-      {
-        'questionText': 'What\'s your favorite car?',
-        'answers':[
-          'Porsche','Lamborghini','Toyota','Ford'
-        ]
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
-              return Answer(_answerQuestion,answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore,_resetQuiz),
       ),
     );
   }
